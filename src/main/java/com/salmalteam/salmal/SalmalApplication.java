542f361d12a1538782872995921454b664e6172d
김규성(LUKAS) <kks01650165@gmail.com>
2023-08-24 22:16:01 +0900

    infra: 백엔드 환경 설정 (#2)
    * infra: 백엔드 환경 설정
    
    * chore: yaml 기본 설정 및 환경 분리
    
    * chore: QueryDSL 의존성 추가 및 gradle 설정
    
    * chore: RestDocs gradle 설정

---
+++ src/main/java/com/salmalteam/salmal/SalmalApplication.java
@@ -0,0 +1,13 @@
+package com.salmalteam.salmal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalmalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalmalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salmalteam/salmal/SalmalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salmalteam.salmal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalmalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
