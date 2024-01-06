c2a5d3f04fae057eef5bf63c290460810840a5a0
kimJaeHyeon <kmr2644@gmail.com>
2024-01-07 03:24:49 +0900

    refactor: HealthCheckController 패키지 이동

--- src/main/java/com/salmalteam/salmal/presentation/http/HealthCheckController.java
+++ src/main/java/com/salmalteam/salmal/presentation/http/HealthCheckController.java
@@ -1,4 +1,4 @@
-package com.salmalteam.salmal;
+package com.salmalteam.salmal.presentation.http;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
