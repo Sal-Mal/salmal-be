f71a202d72f769d52fcd1edfe77b915e00342878
kimJaeHyeon <kmr2644@gmail.com>
2024-01-07 03:35:26 +0900

    refactor: config 패키지 도메인, 인프라로 분리

--- src/main/java/com/salmalteam/salmal/infra/config/JpaConfig.java
+++ src/main/java/com/salmalteam/salmal/infra/config/JpaConfig.java
@@ -1,12 +1,13 @@
-package com.salmalteam.salmal.config;
+package com.salmalteam.salmal.infra.config;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 
 @Configuration
 @EnableJpaAuditing
--- src/main/java/com/salmalteam/salmal/infra/config/S3Config.java
+++ src/main/java/com/salmalteam/salmal/infra/config/S3Config.java
@@ -1,13 +1,14 @@
-package com.salmalteam.salmal.config;
+package com.salmalteam.salmal.infra.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
