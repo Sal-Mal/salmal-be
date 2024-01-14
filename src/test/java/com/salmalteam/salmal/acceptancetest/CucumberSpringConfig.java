75d65d31c230d4775443d65de16ef802ba3e6c9a
kimJaeHyeon <kmr2644@gmail.com>
2024-01-14 22:11:07 +0900

    chore: cucumber config 설정

---
+++ src/test/java/com/salmalteam/salmal/acceptancetest/CucumberSpringConfig.java
@@ -0,0 +1,10 @@
+package com.salmalteam.salmal.acceptancetest;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+
+@CucumberContextConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CucumberSpringConfig {
+}
