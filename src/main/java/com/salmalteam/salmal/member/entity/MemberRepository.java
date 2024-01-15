557dc7092368ba81ff88af804a658acdbc9cfad4
kimJaeHyeon <kmr2644@gmail.com>
2024-01-16 01:43:05 +0900

    refactor: JpaRepository 를 상속하게변경 , 그로인해 사용하지 않는 메서드 삭제

--- src/main/java/com/salmalteam/salmal/member/entity/MemberRepository.java
+++ src/main/java/com/salmalteam/salmal/member/entity/MemberRepository.java
@@ -1,14 +1,11 @@
 package com.salmalteam.salmal.member.entity;
 
-import org.springframework.data.repository.Repository;
-
 import java.util.Optional;
 
-public interface MemberRepository extends Repository <Member, Long>, MemberRepositoryCustom{
-    Member save(Member member);
-    Optional<Member> findById(Long id);
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository <Member, Long>, MemberRepositoryCustom{
     Optional<Member> findByProviderId(String providerId);
     boolean existsByNickName(NickName nickName);
     boolean existsById(Long memberId);
-    void delete(Member member);
 }
