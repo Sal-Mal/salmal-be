0ee8c04355e8cc1841eab5935c37044c34ef63bc
kimJaeHyeon <kmr2644@gmail.com>
2024-01-16 01:52:42 +0900

    feat: 차단한 멤버를 필터링 하는 메서드 구현

--- src/main/java/com/salmalteam/salmal/vote/dto/response/VotePageResponse.java
+++ src/main/java/com/salmalteam/salmal/vote/dto/response/VotePageResponse.java
@@ -1,6 +1,9 @@
 package com.salmalteam.salmal.vote.dto.response;
 
+import static java.util.stream.Collectors.*;
+
 import java.util.List;
+import java.util.function.Predicate;
 
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -10,15 +13,27 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class VotePageResponse {
 
-    private boolean hasNext;
-    private List<VoteResponse> votes;
+	private boolean hasNext;
+	private List<VoteResponse> votes;
 
-    private VotePageResponse(final boolean hasNext,final List<VoteResponse> voteResponses) {
-        this.hasNext = hasNext;
-        this.votes = voteResponses;
-    }
-    public static VotePageResponse of(final boolean hasNext, final List<VoteResponse> voteResponses){
-        return new VotePageResponse(hasNext, voteResponses);
-    }
+	private VotePageResponse(final boolean hasNext, final List<VoteResponse> voteResponses) {
+		this.hasNext = hasNext;
+		this.votes = voteResponses;
+	}
+
+	public static VotePageResponse of(final boolean hasNext, final List<VoteResponse> voteResponses) {
+		return new VotePageResponse(hasNext, voteResponses);
+	}
+
+	public void filteringBlockedMembers(List<Long> ids) {
+		votes = votes.stream()
+			.filter(filterBlockedMemberPredicate(ids))
+			.collect(toList());
+	}
+
+	public Predicate<VoteResponse> filterBlockedMemberPredicate(List<Long> ids) {
+		return voteResponse -> ids.stream()
+			.noneMatch(id -> id.equals(voteResponse.getMemberId()));
+	}
 
 }
