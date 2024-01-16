1517b430d0b52c70768b8856ee99e81fbdec4645
kimJaeHyeon <kmr2644@gmail.com>
2024-01-17 06:53:10 +0900

    refactor: Role enum 어노테이션 필드 추가, 생성메서드 검증로직 추가

--- src/main/java/com/salmalteam/salmal/auth/entity/Role.java
+++ src/main/java/com/salmalteam/salmal/auth/entity/Role.java
@@ -1,11 +1,33 @@
 package com.salmalteam.salmal.auth.entity;
 
+import java.lang.annotation.Annotation;
+
+import com.salmalteam.salmal.auth.annotation.Anonymous;
+import com.salmalteam.salmal.auth.annotation.LoginAdmin;
+import com.salmalteam.salmal.auth.annotation.LoginMember;
+
+import lombok.Getter;
+
+@Getter
 public enum Role {
-	MEMBER,
-	ADMIN,
-	ANONYMOUS;
+	MEMBER(LoginMember.class, "일반회원"),
+	ADMIN(LoginAdmin.class, "어드민"),
+	ANONYMOUS(Anonymous.class, "비회원");
+
+	private final Class<? extends Annotation> annotation;
+	private final String name;
+
+	Role(Class<? extends Annotation> annotation, String name) {
+		this.annotation = annotation;
+		this.name = name;
+	}
 
 	public static Role of(String role) {
-		return Role.of(role);
+		try {
+			return valueOf(role);
+		} catch (NullPointerException | IllegalArgumentException e) {
+			throw new IllegalArgumentException("일치하는 룰이 존재하지 않습니다.");
+		}
 	}
+
 }
