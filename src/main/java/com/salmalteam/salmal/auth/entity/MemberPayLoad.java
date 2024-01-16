ab748c158380e6d9fb10a2ca13b237e4450f00c6
kimJaeHyeon <kmr2644@gmail.com>
2024-01-17 06:36:45 +0900

    refactor: payload 에 role 추가, role 을 포함한 생성메서드 추가(추후 기존에 존재하던 생성 메서드는 삭제예정)

--- src/main/java/com/salmalteam/salmal/auth/entity/MemberPayLoad.java
+++ src/main/java/com/salmalteam/salmal/auth/entity/MemberPayLoad.java
@@ -5,11 +5,24 @@ import lombok.Getter;
 @Getter
 public class MemberPayLoad {
 
-    private final Long id;
-    private MemberPayLoad(final Long id){
-        this.id = id;
-    }
-    public static MemberPayLoad from(final Long id){
-        return new MemberPayLoad(id);
-    }
+	private final Long id;
+	private final Role role;
+
+	private MemberPayLoad(final Long id) {
+		this.id = id;
+		role = Role.ANONYMOUS;
+	}
+
+	private MemberPayLoad(final Long id, Role role) {
+		this.id = id;
+		this.role = role;
+	}
+
+	public static MemberPayLoad from(final Long id) {
+		return new MemberPayLoad(id);
+	}
+
+	public static MemberPayLoad of(final Long id, final Role role) {
+		return new MemberPayLoad(id, role);
+	}
 }
