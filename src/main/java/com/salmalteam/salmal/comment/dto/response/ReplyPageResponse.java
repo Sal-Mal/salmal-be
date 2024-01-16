92621017e0740450f6df64dbdcfe070fb4990cce
kimJaeHyeon <kmr2644@gmail.com>
2024-01-16 10:11:18 +0900

    refactor: 댓글과 대댓글 페이지, 전체 조회시 차단한 회원은 조회되지 않게 수정

--- src/main/java/com/salmalteam/salmal/comment/dto/response/ReplyPageResponse.java
+++ src/main/java/com/salmalteam/salmal/comment/dto/response/ReplyPageResponse.java
@@ -1,20 +1,34 @@
 package com.salmalteam.salmal.comment.dto.response;
 
-import lombok.Getter;
+import static java.util.stream.Collectors.*;
 
 import java.util.List;
+import java.util.function.Predicate;
+
+import lombok.Getter;
 
 @Getter
 public class ReplyPageResponse {
-    private boolean hasNext;
-    private List<ReplyResponse> comments;
+	private boolean hasNext;
+	private List<ReplyResponse> comments;
 
-    private ReplyPageResponse(boolean hashNext, List<ReplyResponse> comments) {
-        this.hasNext = hashNext;
-        this.comments = comments;
-    }
+	private ReplyPageResponse(boolean hashNext, List<ReplyResponse> comments) {
+		this.hasNext = hashNext;
+		this.comments = comments;
+	}
 
-    public static ReplyPageResponse of(boolean hashNext, List<ReplyResponse> comments){
-        return new ReplyPageResponse(hashNext, comments);
-    }
+	public static ReplyPageResponse of(boolean hashNext, List<ReplyResponse> comments) {
+		return new ReplyPageResponse(hashNext, comments);
+	}
+
+	public void filteringBlockedMembers(List<Long> ids) {
+		comments = comments.stream()
+			.filter(filterBlockedMemberPredicate(ids))
+			.collect(toList());
+	}
+
+	private Predicate<ReplyResponse> filterBlockedMemberPredicate(List<Long> ids) {
+		return voteResponse -> ids.stream()
+			.noneMatch(id -> id.equals(voteResponse.getMemberId()));
+	}
 }
