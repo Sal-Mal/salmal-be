417308adef9c131443109f035b8080b558a21188
kimJaeHyeon <kmr2644@gmail.com>
2024-01-26 23:27:05 +0900

    fix: NoArgsConstructor 추가, AddFcmRequest 패키지 변경

--- src/main/java/com/salmalteam/salmal/fcm/dto/request/AddFcmRequest.java
+++ src/main/java/com/salmalteam/salmal/fcm/dto/request/AddFcmRequest.java
@@ -1,11 +1,13 @@
-package com.salmalteam.salmal.notification.dto.request;
+package com.salmalteam.salmal.fcm.dto.request;
 
 import javax.validation.constraints.NotBlank;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 public class AddFcmRequest {
 	@NotBlank(message = "FCM 토큰은 필수적으로 필요합니다.")
