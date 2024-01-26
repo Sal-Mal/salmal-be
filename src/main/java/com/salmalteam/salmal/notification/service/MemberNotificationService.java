91a18a129565a89fc4242e192e7210e503a4e24c
kimJaeHyeon <kmr2644@gmail.com>
2024-01-26 16:30:55 +0900

    feat: FCM 토큰 등록 api 구현

---
+++ src/main/java/com/salmalteam/salmal/notification/service/MemberNotificationService.java
@@ -0,0 +1,10 @@
+package com.salmalteam.salmal.notification.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberNotificationService {
+	public void addToken(Long memberId, String token) {
+
+	}
+}
---
+++ src/main/java/com/salmalteam/salmal/presentation/http/notification/FcmController.java
@@ -0,0 +1,29 @@
+package com.salmalteam.salmal.presentation.http.notification;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.salmalteam.salmal.auth.annotation.LoginMember;
+import com.salmalteam.salmal.auth.entity.MemberPayLoad;
+import com.salmalteam.salmal.notification.dto.request.AddFcmRequest;
+import com.salmalteam.salmal.notification.service.MemberNotificationService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class FcmController {
+	private final MemberNotificationService memberNotificationService;
+
+	@PostMapping("/api/fcm/add-token")
+	public ResponseEntity<?> addFcm(@LoginMember MemberPayLoad memberPayLoad,
+		@RequestBody @Valid AddFcmRequest addFcmRequest) {
+		memberNotificationService.addToken(memberPayLoad.getId(), addFcmRequest.getToken());
+		return ResponseEntity.ok()
+			.build();
+	}
+}
