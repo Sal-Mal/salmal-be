d1572c0a1f2b2866cb42fb729fc05b409f475b93
kimJaeHyeon <kmr2644@gmail.com>
2024-01-26 17:25:21 +0900

    feat: 알림삭제 기능 구현

--- src/main/java/com/salmalteam/salmal/notification/infra/NotificationRepository.java
+++ src/main/java/com/salmalteam/salmal/notification/infra/NotificationRepository.java
@@ -8,4 +8,6 @@ import com.salmalteam.salmal.notification.entity.Notification;
 
 public interface NotificationRepository extends JpaRepository<Notification,Long> {
 	List<Notification> findByMemberId(Long memberId);
+
+	void deleteByUuid(String uuid);
 }
--- src/main/java/com/salmalteam/salmal/notification/service/NotificationService.java
+++ src/main/java/com/salmalteam/salmal/notification/service/NotificationService.java
@@ -3,25 +3,34 @@ package com.salmalteam.salmal.notification.service;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.salmalteam.salmal.member.application.MemberService;
 import com.salmalteam.salmal.notification.dto.response.FindNotificationResponse;
 import com.salmalteam.salmal.notification.dto.response.NotificationDto;
 import com.salmalteam.salmal.notification.infra.NotificationRepository;
 
 import lombok.RequiredArgsConstructor;
-
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
 
 	private final NotificationRepository notificationRepository;
+	private final MemberService memberService;
+
 	public FindNotificationResponse findAll(Long memberId) {
 		return new FindNotificationResponse(notificationRepository.findByMemberId(memberId).stream()
 			.map(NotificationDto::create)
 			.collect(Collectors.toList()));
 	}
 
-	public void delete(Long id, String uuid) {
+	@Transactional
+	public void delete(Long memberId, String uuid) {
+		validateMember(memberId);
+		notificationRepository.deleteByUuid(uuid);
+	}
 
+	private void validateMember(Long memberId) {
+		memberService.findMemberById(memberId);
 	}
 }
