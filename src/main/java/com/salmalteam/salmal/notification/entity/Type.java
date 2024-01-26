a0fcc6e654d7b96c31d6269a6246fe15172b12c3
kimJaeHyeon <kmr2644@gmail.com>
2024-01-26 17:19:31 +0900

    feat: reply Type 추가

--- src/main/java/com/salmalteam/salmal/notification/entity/Type.java
+++ src/main/java/com/salmalteam/salmal/notification/entity/Type.java
@@ -1,4 +1,12 @@
 package com.salmalteam.salmal.notification.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
 public enum Type {
+	REPLY("대댓글 알림");
+
+	private final String description;
 }
