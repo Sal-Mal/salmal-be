8ff687f8669067a8cfcf31b643df2ef510a3a840
kimJaeHyeon <kmr2644@gmail.com>
2024-01-26 20:00:24 +0900

    fix: 파라미터 변경

--- src/main/java/com/salmalteam/salmal/notification/service/NotificationPublisher.java
+++ src/main/java/com/salmalteam/salmal/notification/service/NotificationPublisher.java
@@ -3,5 +3,5 @@ package com.salmalteam.salmal.notification.service;
 import com.salmalteam.salmal.notification.dto.MessageSpec;
 
 public interface NotificationPublisher {
-	void pub(MessageSpec messageSpec, String token);
+	void pub(MessageSpec messageSpec);
 }
