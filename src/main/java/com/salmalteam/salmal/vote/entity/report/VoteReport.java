29824cd09fef5bd823d6c1b86a74401b5d908b44
kimJaeHyeon <kmr2644@gmail.com>
2024-03-14 13:00:00 +0900

    refactor: 신고사유 필드 추가, 생성 메서드 구현

--- src/main/java/com/salmalteam/salmal/vote/entity/report/VoteReport.java
+++ src/main/java/com/salmalteam/salmal/vote/entity/report/VoteReport.java
@@ -1,5 +1,14 @@
 package com.salmalteam.salmal.vote.entity.report;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import com.salmalteam.salmal.common.entity.BaseCreatedTimeEntity;
 import com.salmalteam.salmal.member.entity.Member;
 import com.salmalteam.salmal.vote.entity.Vote;
@@ -8,31 +17,41 @@ import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EqualsAndHashCode(of = {"id"}, callSuper = true)
 @Table(name = "vote_report")
 public class VoteReport extends BaseCreatedTimeEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "vote_id")
-    private Vote vote;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "vote_id")
+	private Vote vote;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "reporter_id")
-    private Member reporter;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "reporter_id")
+	private Member reporter;
 
-    private VoteReport(final Vote vote, final Member member){
-        this.vote = vote;
-        this.reporter = member;
-    }
-    public static VoteReport of(final Vote vote, final Member member){
-        return new VoteReport(vote, member);
-    }
+	private String reason;
+
+	private VoteReport(final Vote vote, final Member member) {
+		this(vote, member, "NONE");
+	}
+
+	private VoteReport(final Vote vote, final Member member, final String reason) {
+		this.vote = vote;
+		this.reporter = member;
+		this.reason = reason;
+	}
+
+	public static VoteReport of(final Vote vote, final Member member) {
+		return new VoteReport(vote, member);
+	}
+
+	public static VoteReport createByReason(final Vote vote, final Member member, final String reason) {
+		return new VoteReport(vote, member, reason);
+	}
 }
