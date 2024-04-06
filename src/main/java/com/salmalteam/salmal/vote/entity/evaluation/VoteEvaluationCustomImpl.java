5a35bb740a8c27907c7bef3b1b1fe2f77adc139b
kimJaeHyeon <kmr2644@gmail.com>
2024-04-06 18:06:20 +0900

    feat: 투표에 참여한 회원 조회 구현,커스텀 인터페이스 작성

---
+++ src/main/java/com/salmalteam/salmal/vote/entity/evaluation/VoteEvaluationCustomImpl.java
@@ -0,0 +1,28 @@
+package com.salmalteam.salmal.vote.entity.evaluation;
+
+import static com.querydsl.core.types.Projections.*;
+import static com.salmalteam.salmal.member.entity.QMember.*;
+import static com.salmalteam.salmal.vote.entity.evaluation.QVoteEvaluation.*;
+
+import java.util.List;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.salmalteam.salmal.vote.dto.response.MemberVoteParticipantsDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class VoteEvaluationCustomImpl implements VoteEvaluationCustom {
+
+	private final JPAQueryFactory jpaQueryFactory;
+
+	@Override
+	public List<MemberVoteParticipantsDto> findVoteMemberVoteParticipants(Long voteId) {
+		return jpaQueryFactory.select(fields(MemberVoteParticipantsDto.class,
+				voteEvaluation.evaluator.id, voteEvaluation.evaluator.nickName.value.as("nickname"),
+				voteEvaluation.evaluator.memberImage.imageUrl.as("profileImageUrl")))
+			.from(voteEvaluation)
+			.leftJoin(voteEvaluation.evaluator, member).on(voteEvaluation.vote.id.eq(voteId))
+			.fetch();
+	}
+}
