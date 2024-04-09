ddbd668fe3f2cf4513939425d3314261d8059b5b
kimJaeHyeon <kmr2644@gmail.com>
2024-04-09 21:24:20 +0900

    test: 테스트 케이스 변경

--- src/test/java/com/salmalteam/salmal/vote/entity/evaluation/VoteEvaluationRepositoryTest.java
+++ src/test/java/com/salmalteam/salmal/vote/entity/evaluation/VoteEvaluationRepositoryTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.annotation.DirtiesContext;
 
 import com.salmalteam.salmal.config.DataJpaTestConfig;
 import com.salmalteam.salmal.member.entity.Member;
@@ -34,35 +33,31 @@ class VoteEvaluationRepositoryTest {
 
 	@BeforeEach
 	void setUp() {
-		memberRepository.save(Member.createActivatedMember("provider111", "nickName1", "KAKAO", true));
-		memberRepository.save(Member.createActivatedMember("provider112", "nickName2", "KAKAO", true));
-		memberRepository.save(Member.createActivatedMember("provider113", "nickName3", "KAKAO", true));
-		Member activatedMember = Member.createActivatedMember("provider114", "nickName4", "KAKAO", true);
+		Member activatedMember = Member.createActivatedMember("provider110", "nickName4", "KAKAO", true);
 		memberRepository.save(activatedMember);
 		voteRepository.save(Vote.of("imageUrl", activatedMember));
 	}
 
 	@Test
-	@DirtiesContext
 	@DisplayName("투표에 참여한 회원정보들을 조회해야한다.")
 	void findVoteMemberVoteParticipants() {
 		//given
 		Vote vote = voteRepository.findById(1L).get();
-		Member member1 = memberRepository.findById(1L).get();
-		Member member2 = memberRepository.findById(2L).get();
-		Member member3 = memberRepository.findById(3L).get();
-		Member member4 = memberRepository.findById(4L).get();
 
-		voteEvaluationRepository.save(VoteEvaluation.of(vote, member1, VoteEvaluationType.LIKE));
-		voteEvaluationRepository.save(VoteEvaluation.of(vote, member2, VoteEvaluationType.LIKE));
-		voteEvaluationRepository.save(VoteEvaluation.of(vote, member3, VoteEvaluationType.LIKE));
-		voteEvaluationRepository.save(VoteEvaluation.of(vote, member4, VoteEvaluationType.LIKE));
+		voteEvaluationRepository.save(VoteEvaluation.of(vote, memberRepository.save(Member.createActivatedMember("provider111", "nickName1", "KAKAO", true)), VoteEvaluationType.LIKE));
+		voteEvaluationRepository.save(VoteEvaluation.of(vote, memberRepository.save(Member.createActivatedMember("provider112", "nickName2", "KAKAO", true)), VoteEvaluationType.LIKE));
+		voteEvaluationRepository.save(VoteEvaluation.of(vote, memberRepository.save(Member.createActivatedMember("provider113", "nickName3", "KAKAO", true)), VoteEvaluationType.LIKE));
 
 		//when
 		List<MemberVoteParticipantsDto> actual = voteEvaluationCustom.findVoteMemberVoteParticipants(1L);
 
 		//then
-		assertThat(actual).extracting(MemberVoteParticipantsDto::getId)
-			.containsOnly(1L, 2L, 3L, 4L);
+		assertThat(actual).extracting(MemberVoteParticipantsDto::getNickname)
+			.containsOnly("nickName1","nickName2","nickName3");
+
+		assertThat(actual).extracting(MemberVoteParticipantsDto::getProfileImageUrl)
+			.containsOnly("https://salmal-image.s3.ap-northeast-2.amazonaws.com/default.png");
+
+
 	}
 }
