3501d1c1359a31d499108f192633a8cd9efa759f
kimJaeHyeon <kmr2644@gmail.com>
2024-05-03 15:27:08 +0900

    fix: dataJpaTest 컨텍스트 pk auto increment 공유문제 수정

--- src/test/java/com/salmalteam/salmal/member/entity/MemberRepositoryTest.java
+++ src/test/java/com/salmalteam/salmal/member/entity/MemberRepositoryTest.java
@@ -2,7 +2,6 @@ package com.salmalteam.salmal.member.entity;
 
 import static org.assertj.core.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,36 +21,75 @@ class MemberRepositoryTest {
 	MemberRepository memberRepository;
 	@Autowired
 	VoteRepository voteRepository;
-
-	@BeforeEach
-	void setUp() {
-
-	}
+	@Autowired
+	MemberBlockedRepository memberBlockedRepository;
 
 	@Test
 	@DisplayName("회원 프로필 정보가 조회되어야 한다.")
-	void searchMyPageV2() throws Exception {
-	    //given
-		Member activatedMember = Member.createActivatedMember("provider110", "nickName4", "KAKAO", true);
-		memberRepository.save(activatedMember);
-		Vote vote1 = Vote.of("imageUrl", activatedMember);
+	void searchMyPageV2() {
+		//given
+		Member searchMember = Member.createActivatedMember("provider110", "nickName4", "KAKAO", true);
+		memberRepository.save(searchMember);
+		Vote vote1 = Vote.of("imageUrl", searchMember);
 		voteRepository.save(vote1);
-		voteRepository.updateVoteEvaluationStatisticsForEvaluationDisLikeInsert(vote1.getId());
-		voteRepository.updateVoteEvaluationStatisticsForEvaluationLikeInsert(vote1.getId());
+		Long searchMemberId = searchMember.getId();
+		Long id = vote1.getId();
 
-		voteRepository.save(Vote.of("imageUrl", activatedMember));
-		voteRepository.save(Vote.of("imageUrl", activatedMember));
-		voteRepository.save(Vote.of("imageUrl", activatedMember));
-		voteRepository.save(Vote.of("imageUrl", activatedMember));
+		voteRepository.updateVoteEvaluationStatisticsForEvaluationDisLikeInsert(id);
+		voteRepository.updateVoteEvaluationStatisticsForEvaluationDisLikeInsert(id);
+		voteRepository.updateVoteEvaluationStatisticsForEvaluationLikeInsert(id);
+
+		voteRepository.save(Vote.of("imageUrl", searchMember));
+		voteRepository.save(Vote.of("imageUrl", searchMember));
+		voteRepository.save(Vote.of("imageUrl", searchMember));
+		voteRepository.save(Vote.of("imageUrl", searchMember));
 
 		//when
-		MyPageV2Response actual = memberRepository.searchMyPageV2(1L, 2L);
+		MyPageV2Response actual = memberRepository.searchMyPageV2(1L, searchMemberId);
 
-	    //then
+		//then
 		assertThat(actual.getTotalVoteCount()).isEqualTo(5);
 		assertThat(actual.getNickName()).isEqualTo("nickName4");
 		assertThat(actual.getImageUrl()).isEqualTo("https://salmal-image.s3.ap-northeast-2.amazonaws.com/default.png");
-		assertThat(actual.getDisLikeCount()).isEqualTo(1);
+		assertThat(actual.isBlocked()).isEqualTo(false);
+		assertThat(actual.getDisLikeCount()).isEqualTo(2);
 		assertThat(actual.getLikeCount()).isEqualTo(1);
+	}
+
+	@Test
+	@DisplayName("회원 프로필 정보가 조회되어야 한다(차단)")
+	void searchMyPageV2_blocked() {
+		//given
+		Member member = Member.createActivatedMember("provider110", "member", "KAKAO", true);
+		Member searchMember = Member.createActivatedMember("provider110", "searchMember", "KAKAO", true);
+		memberRepository.save(member);
+		memberRepository.save(searchMember);
+		memberBlockedRepository.save(MemberBlocked.of(member, searchMember));
+
+		Vote vote1 = Vote.of("imageUrl", searchMember);
+		voteRepository.save(vote1);
+
+		Long memberId = member.getId();
+		Long searchMemberId = searchMember.getId();
+		Long voteId = vote1.getId();
+
+		voteRepository.updateVoteEvaluationStatisticsForEvaluationDisLikeInsert(voteId);
+		voteRepository.updateVoteEvaluationStatisticsForEvaluationLikeInsert(voteId);
+
+		voteRepository.save(Vote.of("imageUrl", searchMember));
+		voteRepository.save(Vote.of("imageUrl", searchMember));
+		voteRepository.save(Vote.of("imageUrl", searchMember));
+		voteRepository.save(Vote.of("imageUrl", searchMember));
+
+		//when
+		MyPageV2Response actual = memberRepository.searchMyPageV2(memberId, searchMemberId);
+
+		//then
+		assertThat(actual.getTotalVoteCount()).isEqualTo(5);
+		assertThat(actual.getNickName()).isEqualTo("searchMember");
+		assertThat(actual.getImageUrl()).isEqualTo("https://salmal-image.s3.ap-northeast-2.amazonaws.com/default.png");
+		assertThat(actual.isBlocked()).isEqualTo(true);
+		assertThat(actual.getLikeCount()).isEqualTo(1);
+		assertThat(actual.getDisLikeCount()).isEqualTo(1);
 	}
 }
--- src/test/java/com/salmalteam/salmal/vote/entity/evaluation/VoteEvaluationRepositoryTest.java
+++ src/test/java/com/salmalteam/salmal/vote/entity/evaluation/VoteEvaluationRepositoryTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,25 +30,22 @@ class VoteEvaluationRepositoryTest {
 	@Autowired
 	VoteEvaluationRepository voteEvaluationRepository;
 
-	@BeforeEach
-	void setUp() {
-		Member activatedMember = Member.createActivatedMember("provider110", "nickName4", "KAKAO", true);
-		memberRepository.save(activatedMember);
-		voteRepository.save(Vote.of("imageUrl", activatedMember));
-	}
-
 	@Test
 	@DisplayName("투표에 참여한 회원정보들을 조회해야한다.")
 	void findVoteMemberVoteParticipants() {
 		//given
-		Vote vote = voteRepository.findById(1L).get();
-
+		Member activatedMember = Member.createActivatedMember("provider110", "nickName4", "KAKAO", true);
+		memberRepository.save(activatedMember);
+		Vote vote = Vote.of("imageUrl", activatedMember);
+		voteRepository.save(vote);
 		voteEvaluationRepository.save(VoteEvaluation.of(vote, memberRepository.save(Member.createActivatedMember("provider111", "nickName1", "KAKAO", true)), VoteEvaluationType.LIKE));
 		voteEvaluationRepository.save(VoteEvaluation.of(vote, memberRepository.save(Member.createActivatedMember("provider112", "nickName2", "KAKAO", true)), VoteEvaluationType.LIKE));
 		voteEvaluationRepository.save(VoteEvaluation.of(vote, memberRepository.save(Member.createActivatedMember("provider113", "nickName3", "KAKAO", true)), VoteEvaluationType.LIKE));
 
+		Long voteId = vote.getId();
+
 		//when
-		List<MemberVoteParticipantsDto> actual = voteEvaluationCustom.findVoteMemberVoteParticipants(1L);
+		List<MemberVoteParticipantsDto> actual = voteEvaluationCustom.findVoteMemberVoteParticipants(voteId);
 
 		//then
 		assertThat(actual).extracting(MemberVoteParticipantsDto::getNickname)
