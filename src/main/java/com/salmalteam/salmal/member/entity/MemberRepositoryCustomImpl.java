357d78347a539a8130c6d41dba8b7f7e02e0ecdb
kimJaeHyeon <kmr2644@gmail.com>
2024-05-03 15:26:28 +0900

    fix: 잘못된 파라미터 사용 변경

--- src/main/java/com/salmalteam/salmal/member/entity/MemberRepositoryCustomImpl.java
+++ src/main/java/com/salmalteam/salmal/member/entity/MemberRepositoryCustomImpl.java
@@ -18,56 +18,56 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
 
-    private final JPAQueryFactory jpaQueryFactory;
+	private final JPAQueryFactory jpaQueryFactory;
 
-    @Override
-    public MyPageResponse searchMyPage(final Long memberId) {
+	@Override
+	public MyPageResponse searchMyPage(final Long memberId) {
 
-        final BooleanExpression blockedMemberSubQuery = JPAExpressions.selectFrom(memberBlocked)
-                .where(
-                        memberBlocked.target.id.eq(memberId)
-                )
-                .exists();
-        return jpaQueryFactory.select(new QMyPageResponse(
-                        member.id,
-                        member.memberImage.imageUrl,
-                        member.nickName.value,
-                        member.introduction.value,
-                        vote.likeCount.sum(),
-                        vote.dislikeCount.sum(),
-                        blockedMemberSubQuery
-                ))
-                .from(member)
-                .where(member.id.eq(memberId))
-                .leftJoin(vote)
-                .on(vote.member.id.eq(memberId))
-                .groupBy(member.id)
-                .fetchOne();
-    }
+		final BooleanExpression blockedMemberSubQuery = JPAExpressions.selectFrom(memberBlocked)
+			.where(
+				memberBlocked.target.id.eq(memberId)
+			)
+			.exists();
+		return jpaQueryFactory.select(new QMyPageResponse(
+				member.id,
+				member.memberImage.imageUrl,
+				member.nickName.value,
+				member.introduction.value,
+				vote.likeCount.sum(),
+				vote.dislikeCount.sum(),
+				blockedMemberSubQuery
+			))
+			.from(member)
+			.where(member.id.eq(memberId))
+			.leftJoin(vote)
+			.on(vote.member.id.eq(memberId))
+			.groupBy(member.id)
+			.fetchOne();
+	}
 
-    @Override
-    public MyPageV2Response searchMyPageV2(final Long memberId, Long searchMemberId) {
-        return jpaQueryFactory.select(
-            new QMyPageV2Response( member.id,
-                member.memberImage.imageUrl,
-                member.nickName.value,
-                member.introduction.value,
-                vote.likeCount.sum().as("likeCount"),
-                vote.dislikeCount.sum().as("dislikeCount"),
-                asBoolean(existsBlockMember(memberId,searchMemberId)),
-                vote.id.count().as("totalCount")))
-            .from(member)
-            .leftJoin(vote)
-            .on(vote.member.id.eq(memberId))
-            .groupBy(member.id)
-            .where(member.id.eq(memberId))
-            .fetchOne();
-    }
+	@Override
+	public MyPageV2Response searchMyPageV2(final Long memberId, Long searchMemberId) {
+		return jpaQueryFactory.select(
+				new QMyPageV2Response(member.id,
+					member.memberImage.imageUrl,
+					member.nickName.value,
+					member.introduction.value,
+					vote.likeCount.sum().as("likeCount"),
+					vote.dislikeCount.sum().as("dislikeCount"),
+					asBoolean(existsBlockMember(memberId, searchMemberId)),
+					vote.id.count().as("totalCount")))
+			.from(member)
+			.leftJoin(vote)
+			.on(vote.member.id.eq(searchMemberId))
+			.groupBy(member.id)
+			.where(member.id.eq(searchMemberId))
+			.fetchOne();
+	}
 
-    private boolean existsBlockMember(Long blockerId, Long targetId) {
-        return jpaQueryFactory.select(memberBlocked.id)
-            .from(memberBlocked)
-            .where(memberBlocked.blocker.id.eq(blockerId), memberBlocked.target.id.eq(targetId))
-            .fetchFirst() != null;
-    }
+	private boolean existsBlockMember(Long blockerId, Long targetId) {
+		return jpaQueryFactory.select(memberBlocked.id)
+			.from(memberBlocked)
+			.where(memberBlocked.blocker.id.eq(blockerId), memberBlocked.target.id.eq(targetId))
+			.fetchFirst() != null;
+	}
 }
